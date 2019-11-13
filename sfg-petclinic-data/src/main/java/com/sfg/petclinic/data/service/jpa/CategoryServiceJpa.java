dde85474628d9a180caac41d7c4d8b895e01a154
Witek <witold.wojnarowicz@gmail.com>
2019-11-13 22:07:58 +0100

    Added JPA Service

---
+++ sfg-petclinic-data/src/main/java/com/sfg/petclinic/data/service/jpa/CategoryServiceJpa.java
@@ -0,0 +1,58 @@
+package com.sfg.petclinic.data.service.jpa;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.sfg.petclinic.data.model.Category;
+import com.sfg.petclinic.data.repository.CategoryRepository;
+import com.sfg.petclinic.data.service.CategoryService;
+
+@Service
+@Profile("jpa")
+public class CategoryServiceJpa implements CategoryService {
+
+	private final CategoryRepository categoryRepository;
+
+	@Autowired
+	public CategoryServiceJpa(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+
+	@Override
+	public Set<Category> findAll() {
+		Set<Category> categories = new HashSet<>();
+		categoryRepository.findAll().iterator().forEachRemaining(categories::add);;
+		return categories;
+	}
+
+	@Override
+	public Category findById(Long id) {
+		Optional<Category> optional = categoryRepository.findById(id);
+		return optional.orElse(null);
+	}
+
+	@Override
+	public Category save(Category category) {
+		return categoryRepository.save(category);
+	}
+
+	@Override
+	public void delete(Category category) {
+		categoryRepository.delete(category);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		categoryRepository.deleteById(id);
+	}
+
+	@Override
+	public Category findByName(String name) {
+		return categoryRepository.findByName(name);
+	}
+}
