ad7543cee2419941f907668ccb390053123af2b1
VVoynarovich <witold.wojnarowicz@gds.ey.com>
2019-12-19 15:54:47 +0100

    Add Vets API for JSON request

--- sfg-petclinic-web/src/main/java/com/sfg/petclinic/web/controller/VetController.java
+++ sfg-petclinic-web/src/main/java/com/sfg/petclinic/web/controller/VetController.java
@@ -1,11 +1,16 @@
 package com.sfg.petclinic.web.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sfg.petclinic.data.model.Vet;
 import com.sfg.petclinic.data.service.VetService;
 
 @Controller
@@ -22,5 +27,11 @@ public class VetController {
     public String getVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index"; 
+    }
+    
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        Set<Vet> vets = vetService.findAll();
+        return vets;
     }
 }
